56c3857c9322138419e9413e93b5bc11b329e008
gregory <greg2502@gmail.com>
2014-08-25 17:35:52 -0700

    print logs to stdout in prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,6 +37,7 @@ Cms::Application.configure do
   # config.log_tags = [ :subdomain, :uuid ]
 
   # Use a different logger for distributed setups
+  config.logger = Logger.new(STDOUT)
   # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
 
   # Use a different cache store in production
