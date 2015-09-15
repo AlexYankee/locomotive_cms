c420c2ebcd2e24bcb80b427a39922c41b6cd2628
Alex Marchenko <amar4enko@gmail.com>
2015-09-16 02:50:22 +0700

    Defaults for redis

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
 #Cms::Application.config.session_store :cookie_store, key: '_cms_session'
-Cms::Application.config.session_store :redis_store, :expires_in => 90.minutes, servers: "redis://#{ENV['REDIS_PORT_6379_TCP_ADDR']}:6379/0/cache"
+Cms::Application.config.session_store :redis_store, :expires_in => 90.minutes, servers: "redis://#{ENV['REDIS_PORT_6379_TCP_ADDR'] || 'localhost'}:6379/0/cache"
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
