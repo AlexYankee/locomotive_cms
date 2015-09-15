49aac647d9a84c08fbc6205eedb1556897c29ab0
Alex Marchenko <amar4enko@gmail.com>
2015-09-16 01:40:59 +0700

    TYPO fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,7 +44,7 @@ Cms::Application.configure do
   # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
 
   # Use a different cache store in production
-  config.cache_store = :reds_store, "redis://#{ENV['REDIS_PORT_6379_TCP_ADDR']}:6379/0/cache", {namespace: Cms, expires_in: 1.day }
+  config.cache_store = :redis_store, "redis://#{ENV['REDIS_PORT_6379_TCP_ADDR']}:6379/0/cache", {namespace: Cms, expires_in: 1.day }
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
   # config.action_controller.asset_host = "http://assets.example.com"
